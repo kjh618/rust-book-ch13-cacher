001093ab857b807411b850d1d38bc794fbd4d02e
Jihoon Kim <kjh618@kaist.ac.kr>
2020-03-06 14:56:54 +0900

    Implement cacher

---
+++ src/cacher.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+
+pub struct Cacher<C, P, R>
+    where
+        C: Fn(P) -> R,
+        P: std::hash::Hash + std::cmp::Eq + Copy,
+        R: Copy {
+    calculation: C,
+    cache: HashMap<P, R>
+}
+
+impl<C, P, R> Cacher<C, P, R>
+    where
+        C: Fn(P) -> R,
+        P: std::hash::Hash + std::cmp::Eq + Copy,
+        R: Copy {
+    pub fn new(calculation: C) -> Cacher<C, P, R> {
+        Cacher {
+            calculation,
+            cache: HashMap::new(),
+        }
+    }
+
+    pub fn value(&mut self, arg: P) -> R {
+        match self.cache.get(&arg) {
+            Some(&v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.cache.insert(arg, v);
+                v
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let calculation = |x| {
+            println!("[calculation({}) called!]", x);
+            x + 1
+        };
+
+        let mut cacher = Cacher::new(calculation);
+        assert_eq!(cacher.value(1), 2);
+        assert_eq!(cacher.value(1), 2);
+        assert_eq!(cacher.value(2), 3);
+    }
+}
